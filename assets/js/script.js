e687672f4192692b034794a546ca32918b14604d
Berilkdd <brlkrdnz@hotmail.com>
2025-08-21 20:23:37 +0100

    Player and computer hand updates with button click

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+  showOnlyLeft('rock');
+  showOnlyRight('rock');
+
+  
+  document.querySelectorAll('.choice-btn').forEach(btn => {
+    btn.addEventListener('click', () => getPlayerChoice(btn.id));
+  });
+});
+
+
+function getPlayerChoice(choice) {
+  
+showOnlyLeft(choice);
+
+const computerChoice = getComputerChoice();
+
+console.log(choice, computerChoice);
+}
+
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const index = Math.floor(Math.random() * choices.length);
+  const choice = choices[index];
+
+  ['rock-right', 'paper-right', 'scissors-right'].forEach(id => {
+  document.getElementById(id).classList.add('hidden');
+  });
+
+  document.getElementById(`${choice}-right`).classList.remove('hidden');
+
+  return choice; 
+}
+
+
+function showOnlyLeft(choice) {
+  const ids = ['rock-left', 'paper-left', 'scissors-left'];
+  ids.forEach(id => document.getElementById(id).classList.add('hidden'));
+  document.getElementById(`${choice}-left`).classList.remove('hidden');
+}
+
+function showOnlyRight(choice) {
+  const ids = ['rock-right', 'paper-right', 'scissors-right'];
+  ids.forEach(id => document.getElementById(id).classList.add('hidden'));
+  document.getElementById(`${choice}-right`).classList.remove('hidden');
+}
+
